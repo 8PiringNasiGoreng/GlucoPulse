252bb1b0b2f1660e767c11f7d1431fa282ed65b9
8PiringNasiGoreng <cponsel7765@gmail.com>
2026-06-27 16:13:35 +0700

    update

--- diabetes_webapp/diabetes_app/App.bat
+++ diabetes_webapp/diabetes_app/App.bat
@@ -4,10 +4,15 @@ echo   Diabetes AI Prediction System
 echo   Memulai aplikasi, harap tunggu...
 echo ================================================
 echo.
-
+ 
 cd /d "%~dp0"
-
+ 
+echo Mengecek dan menginstall dependencies...
+py -m pip install flask joblib numpy scikit-learn --quiet
+ 
+echo.
+echo Menjalankan aplikasi...
 start "" http://localhost:5000
 py app.py
-
+ 
 pause
